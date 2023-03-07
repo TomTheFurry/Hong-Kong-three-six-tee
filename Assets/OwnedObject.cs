b74e39def5c3b2dbccb203ec41223f0ef09514ad
unknown <a1@tyit35655.vtc.hk>
2023-03-07 14:14:09 +0800

    Added grabing objs

--- Assets/OwnedObject.cs
+++ Assets/OwnedObject.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using System.Linq;
 
-public class OwnedObject : MonoBehaviour
+public class OwnedObject : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool HidenByOwners { get; private set; }
+    private GameObject child;
 
-    // Update is called once per frame
+    public void Start() { child = transform.GetChild(0).gameObject; }
+
     void Update()
     {
-        
+        if (HidenByOwners && !photonView.IsMine)
+        {
+            child.GetComponents<Rigidbody>().Select(b => b.isKinematic = false);
+            child.GetComponents<PhotonRigidbodyView>().Select(b => b.enabled = false);
+            child.GetComponents<Collider>().Select(c => c.enabled = false);
+            child.GetComponents<Renderer>().Select(r => r.enabled = false);
+        }
+        else
+        {
+            child.GetComponents<Rigidbody>().Select(b => b.isKinematic = true);
+            child.GetComponents<PhotonRigidbodyView>().Select(b => b.enabled = true);
+            child.GetComponents<Collider>().Select(c => c.enabled = true);
+            child.GetComponents<Renderer>().Select(r => r.enabled = true);
+        }
     }
+
+    public void RPCRevealObject()
+    {
+        photonView.RPC(nameof(RevealOrHideObject), RpcTarget.AllBuffered, false);
+    }
+    public void RPCHideObject()
+    {
+        photonView.RPC(nameof(RevealOrHideObject), RpcTarget.AllBuffered, true);
+    }
+
+    [PunRPC]
+    private void RevealOrHideObject(bool hidden)
+    {
+        Debug.Assert(PhotonNetwork.IsMasterClient, "Must be the master client to control whether object is hidden.");
+        HidenByOwners = hidden;
+    }
+
 }
