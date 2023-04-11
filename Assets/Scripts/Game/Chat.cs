b152c4b2c22fc11208c3810e0a67a3141edfeb69
TomTheFurry <tomlee92502@yahoo.com>
2023-04-11 21:36:50 +0800

    New Feature: Chat! It's here! (Press 'c' for chat!)

---
+++ Assets/Scripts/Game/Chat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+using JetBrains.Annotations;
+
+using Photon.Pun;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+public class Chat : MonoBehaviourPun
+{
+    public static Chat Instance;
+    public UnityEvent<string> OnRecieveMessage = new();
+
+    void Awake()
+    {
+        Assert.IsNull(Instance);
+        Instance = this;
+    }
+
+    public void send(string message)
+    {
+        Debug.Log($"Sending msg {message}");
+        photonView.RPC("MessageSent", RpcTarget.All, message);
+    }
+
+
+    [PunRPC]
+    public void MessageSent(string message, PhotonMessageInfo info)
+    {
+        Debug.Log($"Recieved msg {message}");
+        OnRecieveMessage.Invoke($"{info.Sender.NickName}: {message}");
+    }
+
+}
---
+++ Assets/Scripts/Game/ChatUI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChatUI : MonoBehaviour
+{
+    public TextMeshProUGUI chatText;
+    public TMP_InputField chatInput;
+
+    public InputActionReference StartChatAction;
+
+    private Queue<(float realtime, string message)> messages = new();
+
+    private void Start()
+    {
+        StartChatAction.action.performed += StartChat;
+        Chat.Instance.OnRecieveMessage.AddListener(Receive);
+        chatInput.onEndEdit.AddListener(OnChatInputEndEdit);
+        chatInput.onDeselect.AddListener(CloseChat);
+    }
+
+    private void OnDestroy()
+    {
+        StartChatAction.action.performed -= StartChat;
+    }
+
+    private void StartChat(InputAction.CallbackContext obj)
+    {
+        Debug.Log("StartChat Toggled");
+        if (chatInput.IsActive())
+        {
+            chatInput.gameObject.SetActive(false);
+            chatInput.DeactivateInputField();
+        }
+        else
+        {
+            chatInput.gameObject.SetActive(true);
+            chatInput.Select();
+            chatInput.ActivateInputField();
+        }
+    }
+
+    private void CloseChat(string text)
+    {
+        chatInput.gameObject.SetActive(false);
+    }
+
+    public void OnChatInputEndEdit(string text)
+    {
+        if (text.Length > 0)
+        {
+            Debug.Log($"Send text {text}");
+            Chat.Instance.send(text);
+        }
+        chatInput.text = "";
+        chatInput.gameObject.SetActive(false);
+    }
+
+    public void Receive(string message)
+    {
+        messages.Enqueue((Time.realtimeSinceStartup, message));
+    }
+
+    private void Update()
+    {
+        while (messages.Count > 0 && messages.Peek().realtime < Time.realtimeSinceStartup - 10)
+        {
+            messages.Dequeue();
+        }
+        chatText.text = string.Join("\n", messages.Select(p => p.message));
+    }
+}
