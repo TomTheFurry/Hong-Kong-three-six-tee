db52033980773525475810f06b1f7daebce19d7b
TomTheFurry <tomlee92502@yahoo.com>
2023-04-16 23:29:39 +0800

    rerange stuff

---
+++ Assets/Scripts/Object/GameItem/HeldItem.cs
@@ -0,0 +1,18 @@
+﻿public class HeldItem : GameItem
+{
+    public override bool IsUsable => false;
+
+    public enum Type
+    {
+        ChildOctopus,
+        OldOctopus,
+        MakeYouGetMyChance,
+        PoorsFunding,
+        NoSickness,
+        HalfCostLevelUp,
+        NoPassbyFee
+    }
+    public Type HeldType;
+
+    public override bool IsIllegal => HeldType == Type.PoorsFunding ? CurrentOwner!.Funds > 2000 : Illegal;
+}
--- Assets/Scripts/Object/GameItem/ItemUsable.cs
+++ Assets/Scripts/Object/GameItem/ItemUsable.cs
@@ -291,22 +291,3 @@ public class ItemUsable : ItemBase
         }
     }
 }
-
-public class HeldItem : GameItem
-{
-    public override bool IsUsable => false;
-
-    public enum Type
-    {
-        ChildOctopus,
-        OldOctopus,
-        MakeYouGetMyChance,
-        PoorsFunding,
-        NoSickness,
-        HalfCostLevelUp,
-        NoPassbyFee
-    }
-    public Type HeldType;
-
-    public override bool IsIllegal => HeldType == Type.PoorsFunding ? CurrentOwner!.Funds > 2000 : Illegal;
-}
