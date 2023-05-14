601d1951548e82eb0da7188e7a0ed33299063771
TomTheFurry <tomlee92502@yahoo.com>
2023-05-14 22:16:14 +0800

    Fix stuff

--- Assets/Scripts/UI/PlayerUiShop.cs
+++ Assets/Scripts/UI/PlayerUiShop.cs
@@ -1,11 +1,9 @@
 using cakeslice;
 using JetBrains.Annotations;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Assertions;
 
